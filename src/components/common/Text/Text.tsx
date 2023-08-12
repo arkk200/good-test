098083a70f9f3ed785aec5fcc515d2f9fbc9d9d6
이명재 <arkk20000@gmail.com>
2023-08-12 17:09:11 +0900

    ADD :: Text 컴포넌트에 color props 추가

--- src/components/common/Text/Text.tsx
+++ src/components/common/Text/Text.tsx
@@ -1,13 +1,15 @@
+import { Color, color } from "@/styles/color.style";
 import { Font, FontWeight, font, fontWeight } from "@/styles/font.style";
 import { CSSProperties, ReactNode } from "react";
 import { styled } from "styled-components";
 
 interface PropTypes {
   children: ReactNode;
-  width: CSSProperties["width"];
-  height: CSSProperties["height"];
+  width?: CSSProperties["width"];
+  height?: CSSProperties["height"];
   type: Font;
   weight?: FontWeight;
+  color?: Color;
 }
 
 const Text = ({
@@ -16,9 +18,14 @@ const Text = ({
   height,
   type,
   weight = "bold",
+  color: colorName = "black",
 }: PropTypes) => {
   return (
-    <StyledText style={{ width, height }} type={type} weight={weight}>
+    <StyledText
+      style={{ width, height, color: color[colorName] }}
+      type={type}
+      weight={weight}
+    >
       {children}
     </StyledText>
   );
@@ -29,4 +36,5 @@ export default Text;
 const StyledText = styled.p<{ type: Font; weight: FontWeight }>`
   ${(props) => font[props.type]}
   ${(props) => fontWeight[props.weight]}
+  user-select: none;
 `;
