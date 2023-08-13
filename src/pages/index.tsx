603f28e3fb4d8e956a9fe178430cb94a11f3353b
이명재 <arkk20000@gmail.com>
2023-08-14 00:44:47 +0900

    ADD :: 메인 페이지 퍼블리싱

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import Button from "@/components/common/Button/Button";
+import { Column, Row } from "@/components/common/Flex/Flex";
+import Input from "@/components/common/Input/Input";
 import { color } from "@/styles/color.style";
 import { font } from "@/styles/font.style";
 import { Inter } from "next/font/google";
@@ -7,15 +10,87 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <StyledHome>
+    <Column alignItems="center" gap={30}>
       <Tset>Tset</Tset>
-    </StyledHome>
+      <Form>
+        <Column gap={24}>
+          <Column gap={16}>
+            <Input
+              label="PIN"
+              backgroundColor="main"
+              placeholder="000000"
+              width={246}
+            />
+            <Button
+              backgroundColor="quaternary"
+              color="main"
+              width={246}
+              size="LARGE"
+            >
+              참가하기
+            </Button>
+            <Button
+              backgroundColor="main"
+              color="quaternary"
+              width={246}
+              size="LARGE"
+            >
+              방 만들기
+            </Button>
+          </Column>
+          <Column gap={16}>
+            <Row justifyContent="space-between">
+              <Button
+                backgroundColor="tertiary"
+                color="main"
+                size="SMALL"
+                width={102}
+              >
+                Tset 만들기
+              </Button>
+              <Button
+                backgroundColor="tertiary"
+                color="main"
+                size="SMALL"
+                width={102}
+              >
+                시험 기록보기
+              </Button>
+            </Row>
+            <Row justifyContent="space-between">
+              <Button
+                backgroundColor="tertiary"
+                color="main"
+                size="SMALL"
+                width={124}
+              >
+                내 정보 수정하기
+              </Button>
+              <Button
+                backgroundColor="quaternary"
+                color="main"
+                size="SMALL"
+                width={102}
+              >
+                로그아웃
+              </Button>
+            </Row>
+          </Column>
+        </Column>
+      </Form>
+    </Column>
   );
 }
-
-const StyledHome = styled.div``;
 
 const Tset = styled.p`
   ${font.D1}
   text-shadow: 3px 3px 0px ${color.quaternary};
+`;
+
+const Form = styled.div`
+  padding: 18px 24px 24px;
+  border: 2px solid ${color.black};
+  border-radius: 12px;
+  background: ${color.secondary};
+  box-shadow: 3px 3px 0px 0px ${color.black};
 `;
